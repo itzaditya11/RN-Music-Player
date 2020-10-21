0411457c5dd65e660eff083ab2adb71aa2f76ba8
aditya <imaditya1106@gmail.com>
2020-10-21 11:07:15 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import PlayerScreen from './src/PlayerScreen';
+
+const App = () => {
+  return (
+    <View>
+      <PlayerScreen />
+    </View>
+  );
+};
+
+export default App;
